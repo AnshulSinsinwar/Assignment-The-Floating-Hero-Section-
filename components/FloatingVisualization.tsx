97a26da3df5ecad893c09fd910d5d6f4c175f9a4
AnshulSinsinwar <anisinsinwar02@gmail.com>
2026-05-22 22:57:26 +0530

    remove unused files

--- components/FloatingVisualization.tsx
+++
@@ -1,5 +0,0 @@
-// This file is no longer used and can be safely deleted.
-// The floating components are now managed directly in HeroSection.tsx
-export default function FloatingVisualization() {
-  return null;
-}
